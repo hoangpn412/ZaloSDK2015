7b8448fc769f06ebdc889797aebbfbc6c2d9dc0a
Hung <hoangpn@vng.com.vn>
2015-06-23 15:15:34 +0700

    Initial commit

---
+++ zaloSDK/src/main/java/com/zing/zalo/zalosdk/oauth/ZingMeLoginView.java
@@ -0,0 +1,12 @@
+package com.zing.zalo.zalosdk.oauth;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+
+public class ZingMeLoginView extends ZingMeBaseLoginView{
+		
+	public ZingMeLoginView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
